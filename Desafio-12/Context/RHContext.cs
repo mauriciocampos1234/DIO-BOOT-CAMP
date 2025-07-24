00240a80a1f339365548e986736bf46f009a9ba3
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2025-07-24 13:17:05 -0300

    Subindo Desafio-12 a branch Desafio-12

---
+++ Desafio-12/Context/RHContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TrilhaNetAzureDesafio.Models;
+
+namespace TrilhaNetAzureDesafio.Context
+{
+    public class RHContext : DbContext
+    {
+        public RHContext(DbContextOptions<RHContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+    }
+}
